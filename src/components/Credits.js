2d685c4add15ca377f632d2bab1d89b5fa4ee2ac
Zachary Armantrading <zacharmantrading97@gmail.com>
2024-11-08 16:41:30 -0500

    furthur testing of adding balance to other pages

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,6 +5,7 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Credits = (props) => {
   return (
--- src/components/Debits.js
+++ src/components/Debits.js
@@ -5,6 +5,7 @@ The Debits component contains information for Debits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Debits = (props) => {
   // Create the list of Debit items
